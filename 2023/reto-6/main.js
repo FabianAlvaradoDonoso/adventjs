b9862ea8c172ad69e35f83a3361f5a33cc11ca89
FabianAlvaradoDonoso <fabian.alvarado1008@gmail.com>
2023-12-06 14:33:08 -0300

    ♻️(refactor): Mejora en código día 6

--- 2023/reto-6/main.js
+++ 2023/reto-6/main.js
@@ -1,17 +1,9 @@
 function maxDistance(movements) {
   let derecha = movements.match(/\>/g)?.length ?? 0
   let izquierda = movements.match(/\</g)?.length ?? 0
-  let comodin = movements.match(/\*/g)?.length ?? 0
+  let extra = movements.length - derecha - izquierda
 
-  let max = Math.max(derecha, izquierda)
-
-  if (derecha === max) {
-    derecha += comodin
-  } else {
-    izquierda += comodin
-  }
-
-  return Math.abs(derecha - izquierda)
+  return Math.abs(derecha - izquierda) + extra
 }
 
 module.exports = maxDistance
